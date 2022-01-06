457aeecabb74967df8f66ac3a0b1027ee818fc05
limarenkodima <limarenko.it22@mail.ru>
2022-01-06 20:03:51 +0300

    Починил удаление элемента без обновления страницы

--- tensor-js-app/src/components/StudentsPage.js
+++ tensor-js-app/src/components/StudentsPage.js
@@ -1,22 +1,40 @@
-import UserCard from "./UserCard.js";
 import styled from "styled-components";
 import { Student } from "./Student.js";
 import read from "../actions/read.js";
 import React from "react";
 import create from "../actions/create.js";
 import uuid from "react-uuid/uuid.js";
+import Delete from "../actions/delete.js";
 
 class StudentsPage extends React.Component {
   constructor(props) {
     super(props);
     this._isMounted = false;
     this.students = [];
-    this.state = { students: this.students, creating: false };
+    this.state = { students: this.students, creating: false, deleting: "" };
+  }
+
+  async componentDidUpdate() {
+    if (this.state.deleting.length > 1) {
+      await this.DeleteStudent(this.state.deleting);
+      await this.updStudents();
+      this.forceUpdate();
+    }
+  }
+
+  async DeleteStudent(uuid) {
+    await Delete(uuid);
+    let studentIndex = this.state.students.findIndex(
+      (item) => item.uuid == uuid
+    );
+    let tempArray = this.state.students.splice(studentIndex, 1);
+    this.setState({ students: tempArray });
+    this.setState({ deleting: "" });
   }
 
   async componentDidMount() {
     this._isMounted = true;
-    if (this._isMounted && !this.state.creating) this.updStudents();
+    this.updStudents();
   }
 
   componentWillUnmount() {
@@ -45,10 +63,48 @@ class StudentsPage extends React.Component {
     this.setState({ creating: false });
   }
 
+  UserCard = (props) => {
+    if (props.name.length > 15)
+      return (
+        <Card>
+          <Avatar ava={props.avatar} />
+          <Info>
+            <SubFrame>
+              <a>{props.name.substr(0, 12) + "..."}</a>
+              <SubInfo>
+                <a>{props.university}</a>
+                <a>{props.city}</a>
+              </SubInfo>
+            </SubFrame>
+            <Trash
+              onClick={() => {
+                this.setState({ deleting: props.uuid });
+              }}
+            />
+          </Info>
+        </Card>
+      );
+    return (
+      <Card>
+        <Avatar ava={props.avatar} />
+        <Info>
+          <SubFrame>
+            <a>{props.name}</a>
+            <SubInfo>
+              <a>{props.university}</a>
+              <a>{props.city}</a>
+            </SubInfo>
+          </SubFrame>
+          <Trash onClick={() => this.setState({ deleting: props.uuid })} />
+        </Info>
+      </Card>
+    );
+  };
+
   OutputListStudents = () => {
     return this.state.students.map((student, index) => (
       <li key={index}>
-        <UserCard
+        <this.UserCard
           uuid={student.uuid}
           name={student.name}
           city={student.city}
@@ -186,7 +242,7 @@ const EmptyStudent = styled.div`
   margin: 0px 0px;
 
   border-width: 2px;
-  border-style: dotted; 
+  border-style: dotted;
   border-color: #7388a8;
   background: white;
 
@@ -244,10 +300,10 @@ const FormPage = styled.div`
     box-sizing: border-box;
   }
   input[type="text"] {
-    font-family: inherit; 
-    font-size: inherit; 
-    line-height: inherit; 
-    margin: 0; 
+    font-family: inherit;
+    font-size: inherit;
+    line-height: inherit;
+    margin: 0;
   }
   .text-field {
     margin-bottom: 1rem;
@@ -257,7 +313,7 @@ const FormPage = styled.div`
     display: block;
     margin-bottom: 0.25rem;
   }
-  
+
   .text-field__input {
     display: block;
     width: 100%;
@@ -286,4 +342,145 @@ const FormPage = styled.div`
     box-shadow: 0 0 0 0.2rem rgba(158, 158, 158, 0.25);
   }
 `;
+
+const Card = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: flex-start;
+
+  position: static;
+  width: 392px;
+  height: 105px;
+  border-radius: 8px;
+
+  flex: none;
+  order: 0;
+  flex-grow: 0;
+  margin: 0px 0px;
+
+  :hover {
+    background: #eef8fe;
+  }
+`;
+
+const Avatar = styled.div`
+  position: static;
+  width: 80px;
+  height: 80px;
+
+  background: center no-repeat url(${({ ava }) => ava});
+  object-fit: cover;
+  border-radius: 8px;
+
+  flex: none;
+  order: 0;
+  flex-grow: 0;
+  margin: 12px 12px;
+`;
+
+const Info = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  padding: 0px;
+
+  position: static;
+  width: 284px;
+  height: 80px;
+
+  flex: none;
+  order: 1;
+  flex-grow: 0;
+  margin: 12px 12px;
+`;
+
+const SubFrame = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  padding: 0px;
+
+  position: static;
+  width: 212px;
+  height: 50px;
+
+  /* Inside Auto Layout */
+
+  flex: none;
+  order: 0;
+  flex-grow: 0;
+
+  a {
+    position: static;
+    width: 212px;
+    height: 29px;
+
+    font-family: "SF UI Display";
+    font-weight: 600;
+    font-size: 24px;
+    line-height: 29px;
+    text-align: left;
+
+    color: #333333;
+
+    flex: none;
+    order: 0;
+    flex-grow: 0;
+  }
+`;
+
+const SubInfo = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: flex-start;
+  padding: 0px;
+
+  position: static;
+  width: 137px;
+  height: 17px;
+
+  /* Inside Auto Layout */
+
+  flex: none;
+  order: 1;
+  flex-grow: 0;
+
+  a {
+    position: static;
+    width: 84px;
+
+    font-family: "SF UI Display";
+    font-style: normal;
+    font-weight: normal;
+    font-size: 12px;
+    line-height: 17px;
+    color: #7a91b5;
+
+    flex: none;
+    order: 0;
+    flex-grow: 0;
+    margin: 0px 12px 0px 0px;
+  }
+`;
+
+const Trash = styled.div`
+  position: static;
+  width: 24px;
+  height: 24px;
+
+  background: url("/images/Trash.svg") no-repeat;
+  border-radius: 8px;
+  border-color: white;
+  opacity: 0.2;
+
+  flex: none;
+  order: 2;
+  flex-grow: 0;
+  margin: 12px 12px;
+
+  :hover {
+    opacity: 1;
+  }
+`;
+
 export default StudentsPage;
